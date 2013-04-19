09ee28939aa9ad0c18a22f8909cbecfbf684c759
Chris Mann <christophermmann@gmail.com>
2013-04-19 11:22:02 +0100

    Added a curlopt allowing cURL to NOT verify the peer (to save people having to import the CA certificate

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
   $jsonData = json_encode($data);
 
   // set the request type to post and the postfields to our json data
+  curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,  false);
   curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST,   "POST");
   curl_setopt($curlHandle, CURLOPT_POSTFIELDS,      $jsonData);
   curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER,  true);
@@ -33,7 +34,6 @@
   // each call to the API returns an HTTP status code,
   // which we can use to determine if the call was succesful
   $status = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
-
   // and if your call should return some json data, you can decode it from
   // the $result variable
   $resultObject = json_decode($result);
@@ -45,7 +45,7 @@
 
   // initialise a second curl handle, this time with the endpoint being Mr. Ginsberg's uid,
   // this will fetch Mr. Ginsberg's record from the users collection
-  $curlHandle = curl_init("http://api.project-ginsberg.com:2404/users/".$resultObject->uid);
+  $curlHandle = curl_init("https://api.project-ginsberg.com/users/".$resultObject->uid);
 
   // but we can't let just anyone fetch Mr. Ginsbergs email address, and first and last name,
   // so we have to send along a cookie to authenticate with the API
@@ -53,6 +53,7 @@
   // logging in generated a cookie session id, which we are now going to send along
   // with the call
   curl_setopt($curlHandle, CURLOPT_COOKIE, "sid=".$resultObject->id."; ");
+  curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
   // you don't need to send the authentication cookie along with every call,
   // but you do need to send it along for most calls
 
@@ -65,10 +66,11 @@
   // initialise a third curl handle
   // this time the endpoint is the obj-fitness collection, but there's also a query being sent
   // in the URL in the form of JSON
-  $curlHandle = curl_init("http://api.project-ginsberg.com:2404/obj-fitness?{\"uid\":\"".$resultObject->uid."\"}");
+  $curlHandle = curl_init("https://api.project-ginsberg.com/obj-fitness?{\"uid\":\"".$resultObject->uid."\"}");
 
   // you won't be able to search collections by uid or see the uid's in them unless you send authentication
   curl_setopt($curlHandle, CURLOPT_COOKIE, "sid=".$resultObject->id."; ");
+  curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
   $output = curl_exec($curlHandle);
   $status = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
   echo("<h4>".$status."</h4>");
@@ -89,7 +91,7 @@
 
   // initialise a final curl handler, the endpoint being the collection
   // to which you want to post
-  $curlHandle = curl_init("http://api.project-ginsberg.com:2404/obj-fitness");
+  $curlHandle = curl_init("https://api.project-ginsberg.com/obj-fitness");
 
   // you will not be able to associate a record with a user unless you send authentication
   curl_setopt($curlHandle, CURLOPT_COOKIE,          "sid=".$resultObject->id."; ");
@@ -97,6 +99,7 @@
   curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST,   "POST");
   curl_setopt($curlHandle, CURLOPT_POSTFIELDS,      $testFitnessDataJson);
   curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER,  true);
+  curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,  false);
   curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
     'Content-Length: ' . strlen($testFitnessDataJson))
